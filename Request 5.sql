b026486c8b2ffe784ddfcaa11e3098a6c230ccbd
rashi0422 <114216858+rashi0422@users.noreply.github.com>
2023-11-03 11:28:33 +0530

    Delete Request 5.sql

--- Request 5.sql
+++
@@ -1,9 +0,0 @@
-# 5. Get the products that have the highest and lowest manufacturing costs #
-
-(select d.product_code , d.product, f.manufacturing_cost from fact_manufacturing_cost f
-join dim_product d on f.product_code = d.product_code 
-group by d.product order by f.manufacturing_cost desc limit 1 )
-UNION
-(select d.product_code , d.product, f.manufacturing_cost from fact_manufacturing_cost f
-join dim_product d on f.product_code = d.product_code 
-group by d.product order by f.manufacturing_cost asc limit 1 );
