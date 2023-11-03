beda12da9855f29a6f5ba39de8c6815a021cbb03
rashi0422 <114216858+rashi0422@users.noreply.github.com>
2023-11-03 11:28:54 +0530

    Delete Request 3.sql

--- Request 3.sql
+++
@@ -1,6 +0,0 @@
-# 3. Provide a report with all the unique product counts for each segment and sort them in descending order of product counts #
-
-select segment , count(product) as 'product_count' 
-from dim_product 
-group by segment 
-order by product_count desc;
